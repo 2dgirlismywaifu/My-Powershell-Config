d0818091b53ee2f1ebf8d8d6d68bcb84344ca706
2dgirlismywaifu <ngothanhlong19943@gmail.com>
2024-03-20 23:12:53 +0700

    add submodule Catppuccin

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -4,8 +4,23 @@
 # ls icon list using Terminal-Icons
 Import-Module -Name Terminal-Icons
 
-# Oh My Posh Theme
-oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/catppuccin_mocha.omp.json" | Invoke-Expression
+# Import the module Catppuccin
+Import-Module Catppuccin
+
+#Imports PSReadLine
+Import-Module PSReadLine
+
+# Set a flavor for easy access
+$Flavor = $Catppuccin['Mocha']
+
+# Print a summary of the flavor's colors
+# Returns Null, calls Write-Host internally.
+$Flavor.Table()
+
+# Print blocks of the flavor's colors
+# Returns a string
+#Write-Host $Flavor.Blocks()
+
 # Import the Chocolatey Profile that contains the necessary code to enable
 # tab-completions to function for `choco`.
 # Be aware that if you are missing these lines from your profile, tab completion
@@ -15,8 +30,7 @@ $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
 if (Test-Path($ChocolateyProfile)) {
   Import-Module "$ChocolateyProfile"
 }
-#Imports PSReadLine
-Import-Module PSReadLine
+
 #Tab - Gives a menu of suggestions
 Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
 #UpArrow will show the most recent command
@@ -30,18 +44,51 @@ Set-PSReadLineOption -ShowToolTips
 #Gives completions/suggestions from historical commands
 Set-PSReadLineOption -PredictionSource History
 #Change the color of the suggestion menu
-Set-PSReadLineOption -Colors @{
-  InlinePrediction = '#a6e3a1'
-  Command            = '#89b4fa'
-  Number             = '#eba0ac'
-  Member             = '#f38ba8'
-  Operator           = '#cba6f7'
-  Type               = '#f2cdcd'
-  Variable           = '#f38ba8'
-  Parameter          = '#f38ba8'
-  ContinuationPrompt = '#f38ba8'
-  Default            = '#b4befe'
-  }
+$Colors = @{
+	# Largely based on the Code Editor style guide
+	# Emphasis, ListPrediction and ListPredictionSelected are inspired by the Catppuccin fzf theme
+
+	# Powershell colours
+	ContinuationPrompt     = $Flavor.Teal.Foreground()
+	Emphasis               = $Flavor.Red.Foreground()
+	Selection              = $Flavor.Surface0.Background()
+
+	# PSReadLine prediction colours
+	InlinePrediction       = $Flavor.Overlay0.Foreground()
+	ListPrediction         = $Flavor.Mauve.Foreground()
+	ListPredictionSelected = $Flavor.Surface0.Background()
+
+	# Syntax highlighting
+	Command                = $Flavor.Blue.Foreground()
+	Comment                = $Flavor.Overlay0.Foreground()
+	Default                = $Flavor.Text.Foreground()
+	Error                  = $Flavor.Red.Foreground()
+	Keyword                = $Flavor.Mauve.Foreground()
+	Member                 = $Flavor.Rosewater.Foreground()
+	Number                 = $Flavor.Peach.Foreground()
+	Operator               = $Flavor.Sky.Foreground()
+	Parameter              = $Flavor.Pink.Foreground()
+	String                 = $Flavor.Green.Foreground()
+	Type                   = $Flavor.Yellow.Foreground()
+	Variable               = $Flavor.Lavender.Foreground()
+}
+
+# Set the colours
+Set-PSReadLineOption -Colors $Colors
+
+# The following colors are used by PowerShell's formatting
+# Again PS 7.2+ only
+$PSStyle.Formatting.Debug = $Flavor.Sky.Foreground()
+$PSStyle.Formatting.Error = $Flavor.Red.Foreground()
+$PSStyle.Formatting.ErrorAccent = $Flavor.Blue.Foreground()
+$PSStyle.Formatting.FormatAccent = $Flavor.Teal.Foreground()
+$PSStyle.Formatting.TableHeader = $Flavor.Rosewater.Foreground()
+$PSStyle.Formatting.Verbose = $Flavor.Yellow.Foreground()
+$PSStyle.Formatting.Warning = $Flavor.Peach.Foreground()
+
+# Oh My Posh Theme
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/catppuccin_mocha.omp.json" | Invoke-Expression
+
 #Change location of history file
 Set-PSReadLineOption -HistorySavePath "$env:USERPROFILE\OneDrive\Documents\PowerShell\history.txt"
 #34de4b3d-13a8-4540-b76d-b9e8d3851756 PowerToys CommandNotFound module
